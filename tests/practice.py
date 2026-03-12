b836be4e41c7e95de38aab6c5a6c62cc090f3a3f
srikanth728264 <sreekanthnaidu558@gmail.com>
2026-03-12 22:29:03 +0530

    added new files

---
+++ tests/practice.py
@@ -0,0 +1,55 @@
+# a = 10 
+# a = 20 
+# print(a)
+
+# #Single data types 
+# #int    1,2,3,4,5,6,7,8,9,
+# #float  9.0,10.1
+# #string    "1",'12.6','True'
+# #boolean   True , False 
+
+# #Complex    a+bj  
+# #Multiple data types 
+
+# x = "Srikanth Muttineni"
+# print(x)
+# a = 10 
+# b = 10.1
+# c = True 
+# d = 'sri'
+
+
+# print(type(a))
+# print(type(b))
+# print(type(c))
+# print(type(d))
+
+
+# #Type casting
+# a = True
+# b = str(a)
+# print(f"Type of b {type(b)}")
+# print(f"Type of a {type(a)}")
+# print(f"the value of b {b}, the type of b {type(b)}")
+
+
+
+# list = []  - # we can store any data types 
+# # [1,2,3,4,5,6] # ['a','b','c','d'] # [2,7,9.1,'Sri',True,False] 
+
+# tuple = () - # (1,2,3,0.1,'r',True)
+# dict  = {} - # key -value pair 
+# x = {"name" : 'Srikanth',"age":26,"Company":"Scryai","Organization":"Scryai"}
+
+
+
+# set  = {}  - # I won't allow 
+
+
+
+# x = 1,2 
+
+
+
+
+
---
+++ tests/test_login.py
@@ -0,0 +1,3 @@
+def test_login(page) :
+    page.goto("https://wwww.facebook.com")
+    page.wait_for_timeout(20000)
